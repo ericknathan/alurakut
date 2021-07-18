8fdc17c4523831b2fdff7ab8bcdff1f88bc67df3
Erick Nathan <erick.capito@hotmail.com>
2021-07-17 23:38:03 -0300

    Aula 3 concluida!

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,18 @@ import MainGrid from '../src/components/MainGrid';
 import Box from '../src/components/Box';
 import { AlurakutMenu, AlurakutProfileSidebarMenuDefault, OrkutNostalgicIconSet } from '../src/lib/AlurakutCommons';
 import { ProfileRelationsBoxWrapper } from '../src/components/ProfileRelations';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+
+function showMore(arr, redirect) {
+  if(arr.length > 6) {
+    return (
+      <div className="show-more">
+        <hr />
+        <a href={redirect}>Ver todos</a>
+      </div>
+    )
+  } else return null
+}
 
 function ProfileUser(props) {
   return (
@@ -22,9 +33,32 @@ function ProfileUser(props) {
   )
 }
 
+function ProfileRelationsBox(props) {
+  return (
+    <ProfileRelationsBoxWrapper>
+      { console.log(props.items) }
+      <h2 className="smallTitle">{props.title} ({props.items.length})</h2>
+      <ul>
+        {props.items.slice(0, 6).map((currentIndex) => {
+            return (
+              <li key={currentIndex.id}>
+                <a href={`/user/${currentIndex.login}`}>
+                  <img src={`https://github.com/${currentIndex.login}.png`} alt={`Foto do seguidor ${currentIndex.login}`}/>
+                  <span>{currentIndex.login}</span>
+                </a>
+              </li>
+            )
+          })
+        }
+      </ul>
+      { showMore(props.items, `/followers`) }
+    </ProfileRelationsBoxWrapper>
+  )
+}
+
 export default function Home() {
   const githubUser = 'ericknathan';
-  
+
   const [communities, setCommunities] = useState([{
     title: 'Eu odeio acordar cedo',
     image: 'https://alurakut.vercel.app/capa-comunidade-01.jpg',
@@ -34,6 +68,18 @@ export default function Home() {
   const favoriteUsers = [
     'juunegreiros', 'omariosouto', 'rafaballerini', 'marcobrunodev', 'felipefialho', 'filipedeschamps', 'guilhermesilveira'
   ];
+
+  const [followers, setFollowers] = useState([]);
+  useEffect(() => {
+    fetch('https://api.github.com/users/ericknathan/followers')
+    .then(serverResponse => {
+      return serverResponse.json();
+    })
+    .then(completeResponse => {
+      setFollowers(completeResponse);
+      return console.log(completeResponse);
+    });
+  }, [followers])
 
   function handleCreateCommunity(e) {
     e.preventDefault();
@@ -45,18 +91,6 @@ export default function Home() {
       id: new Date().toString()
     }]);
   }
-
-  function showMore(arr, redirect) {
-    if(arr.length > 6) {
-      return (
-        <div className="show-more">
-          <hr />
-          <a href={redirect}>Ver todos</a>
-        </div>
-      )
-    } else return null
-  }
-    
 
   return (
     <>
@@ -95,6 +129,7 @@ export default function Home() {
           </Box>
         </div>
         <div className="profileRelationsArea" style={{ gridArea: 'profileRelationsArea' }}>
+          <ProfileRelationsBox title="Seguidores" items={followers}/>
           <ProfileRelationsBoxWrapper>
           <h2 className="smallTitle">Comunidades ({communities.length})</h2>
             <ul>
